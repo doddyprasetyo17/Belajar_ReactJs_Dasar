c3309616c5a032d16ffb21bd7977c31101b99a24
Doddy Prasetyo <65611674+doddyprasetyo17@users.noreply.github.com>
2021-04-01 00:09:12 +0700

    menambahkan link css dan crud

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Crud from './crud';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Crud />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
